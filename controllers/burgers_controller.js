ab29d05a29139d6dcc6eab078e4807eee0d78750
Sankung Sillah <ssillah306@gmail.com>
2021-01-04 17:40:20 -0500

    created routes for the app

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const router = express.Router();
+const burger = require("../models/burger");
+
+router.get("/", (req, res) => {
+  // console.log(req);
+  burger.selectAll((data) => {
+    const hbsObject = {
+      burger: data,
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+  });
+});
+
+router.post("/api/burger", (req, res) => {
+  burger.insertOne("burger_name", req.body.burger_name, (result) => {
+    res.json({ id: result.insertId });
+  });
+});
+
+router.put("/api/burger/:id", (req, res) => {
+
+  let id = req.params.id;
+
+  burger.updateOne(id, (result) => {
+    if (result.changedRows == 0) {
+      // if there's no id found send a 404 message
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
+  });
+});
+
+
+module.exports = router;
