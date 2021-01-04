40d68cef1f21ac7aa1f60112b607724f90ce9bc9
Sankung Sillah <ssillah306@gmail.com>
2021-01-04 16:19:52 -0500

    added our seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burger (burger_name, devoured) VALUES ('Luger Burger', false);
+INSERT INTO burger (burger_name, devoured) VALUES ('Double Animal Style', false);
+INSERT INTO burger (burger_name, devoured) VALUES ('Dyers Deep-Fried Burger', true);
+INSERT INTO burger (burger_name, devoured) VALUES ('Chargrilled Burger with Roquefort Cheese', true);
+INSERT INTO burger (burger_name, devoured) VALUES ('The Lola Burger', true);
